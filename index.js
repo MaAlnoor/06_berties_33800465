11c8082196bbcb0ec1407fa841a8436d791400f7
Mohammed Alnoor <mohammed1169@hotmail.co.uk>
2025-11-20 22:56:28 +0000

    added dotenv

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require ('express')
 var ejs = require('ejs')
 const path = require('path')
 var mysql = require('mysql2');
+require('dotenv').config();
 
 // Create the express application object
 const app = express()
@@ -22,15 +23,16 @@ app.locals.shopData = {shopName: "Bertie's Books"}
 
 // Define the database connection pool
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'berties_books_app',
-    password: 'qwertyuiop',
-    database: 'berties_books',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
 });
 global.db = db;
+
 
 // Load the route handlers
 const mainRoutes = require("./routes/main")
